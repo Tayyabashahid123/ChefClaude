9ce8dbbf07afdc13ada96a31da694556dd6e8454
Tayyaba Shahid <tayyabashahid019@gmail.com>
2025-08-09 00:12:32 +0500

    fix

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,7 @@ import './main.css'
 import { useState} from 'react'
 import ClaudeRecipe from './ClaudeRecipe'
 import IngredientsList from './IngredientsList'
-import { getRecipeFromChefClaude, getRecipeFromMistral } from "../ai"
+import { getRecipeFromMistral } from "../ai"
 
 export default function Main(){
     const [ingredients, setIngredient] = useState([])
@@ -44,27 +44,3 @@ export default function Main(){
 }
 
 
-
-
-
-    /**
-     * Challenge: clean up our code!
-     * Let's make a couple new components to make things a
-     * little cleaner. (Notice: I'm not suggesting what we
-     * have now is bad or wrong. I'm mostly finding an excuse
-     * to get in some hands-on practice 🙂)
-     * 
-     * 1. Move the entire recipe <section> into its own
-     *    ClaudeRecipe component
-     * 2. Move the list of ingredients <section> into its
-     *    own IngredientsList component.
-     * 
-     * While you're considering how to structure things, consider
-     * where state is, think about if it makes sense or not to
-     * move it somewhere else, how you'll communicate between
-     * the parent/child components, etc.
-     * 
-     * The app should function as it currently does when you're
-     * done, so there will likely be some extra work to be done
-     * beyond what I've listed above.
-     */
