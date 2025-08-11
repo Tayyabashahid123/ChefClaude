d86c3366ddfbd6a7594ccb7148d536e98ad04681
Tayyaba Shahid <tayyabashahid019@gmail.com>
2025-08-11 18:54:48 +0500

    added scrollintoview when recipe loads

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -5,7 +5,7 @@ export default function IngredientsList(props){
                     <ul className="ingredients-list" aria-live="polite">{props.list}</ul>
                     {props.list.length > 3 &&
                         <div className="get-recipe-container">
-                            <div>
+                            <div ref={props.ref}>
                                 <h3>Ready for a recipe?</h3>
                                 <p>Generate a recipe from your list of ingredients.</p>
                             </div>
--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import './main.css'
-import { useState} from 'react'
+import { useState, useRef, useEffect} from 'react'
 import ClaudeRecipe from './ClaudeRecipe'
 import IngredientsList from './IngredientsList'
 import { getRecipeFromMistral } from "../ai"
@@ -7,6 +7,7 @@ import { getRecipeFromMistral } from "../ai"
 export default function Main(){
     const [ingredients, setIngredient] = useState([])
     const [recipeShown, setRecipeShown] = useState([])
+    const RecipeSection = useRef(null)
 
     function handleOnSubmit(formData){
         const newIngredient =formData.get("ingredient")
@@ -19,15 +20,23 @@ export default function Main(){
             setIngredient(prevIngredients => [...prevIngredients , newIngredient])
     }
 
+
+
     async function getrecipe(){
-        // setRecipeShown(prevState => !prevState)
-        // console.log(recipeShown)
         const recipeMarkdown = await getRecipeFromMistral(ingredients)
         console.log(recipeMarkdown)
         setRecipeShown(recipeMarkdown)
     }
 
     const ingredientlist= ingredients.map(ingredient => <li key={ingredient}> {ingredient} </li>)
+
+    console.log(RecipeSection)
+    useEffect(() => {
+        if(recipeShown != [] && RecipeSection.current != null ){
+        console.log("recipe shown")
+        RecipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipeShown])
 
     return <>
         <main>
@@ -36,11 +45,17 @@ export default function Main(){
                 <button> Add ingredient</button>
             </form>
             <div className="main-layout-area">
-                {ingredientlist.length > 0 && <IngredientsList list= {ingredientlist} getrecipe= {getrecipe} /> }
+                {ingredientlist.length > 0 && <IngredientsList ref={RecipeSection} list= {ingredientlist} getrecipe= {getrecipe} /> }
             </div>
-            { <ClaudeRecipe recipe={recipeShown}/> }
+            { <ClaudeRecipe recipe={recipeShown} /> }
         </main>
     </>
 }
 
 
+    /**
+     * Problem:
+     * We want to scroll the "Ready for a recipe?" div into view
+     * ONLY AFTER the ClaudeRecipe section is rendered to the page 
+     * (i.e. when `recipe` is not an empty string). How can we do that?
+     */
